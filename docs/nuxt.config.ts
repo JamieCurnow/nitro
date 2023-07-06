3b2875fc778598d2f18c0c56d8f4a6db84eea4ef
JamieCurnow <jamie@island-web.ca>
2023-07-06 17:58:08 +0100

    testing fix

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,8 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus',
-  modules: ['@nuxtjs/plausible', '@nuxthq/studio']
-})
+  extends: "@nuxt-themes/docus",
+  modules: ["@nuxtjs/plausible", "@nuxthq/studio"],
+  experimental: {
+    // test
+    payloadExtraction: false,
+  },
+});
