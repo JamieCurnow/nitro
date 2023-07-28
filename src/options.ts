da60ec7626646517fafa882a8323aff2f3a228b9
JamieCurnow <jamie@island-web.ca>
2023-07-28 19:55:51 +0100

    add gen optional type and default and support for node version selection.

--- src/options.ts
+++ src/options.ts
@@ -119,6 +119,11 @@ const NitroDefaults: NitroConfig = {
   nodeModulesDirs: [],
   hooks: {},
   commands: {},
+
+  // Presets
+  firebase: {
+    gen: 2,
+  },
 };
 
 export interface LoadConfigOptions {
